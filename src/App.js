1c7e22d8d53efd1d88e35355ae7bb6524d18caef
unknown <Ashwiniachu11.com>
2024-03-13 06:47:41 +0530

    uploading

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import WeatherApp from "../src/components/weather app/WeatherApp";
+
+function App(){
+  return(
+  <div>
+<WeatherApp/>
+  </div>
+  )
+}
+export default App;
+
